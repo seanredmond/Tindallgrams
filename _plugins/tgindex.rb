afddbd063dddaab47d2d135cfa9252bb97f6ea66
Sean Redmond <github-smr@sneakemail.com>
2013-07-22 23:56:50 -0400

    Add filter by year to tag; layout for indices

--- _plugins/tgindex.rb
+++ _plugins/tgindex.rb
@@ -4,25 +4,44 @@ module Jekyll
   class TindallgramIndexTag < Liquid::Tag
     def initialize(tag_name, markup, tokens)
       super
+      @grams = YAML.load_file('_data/tindallgrams-index.yaml')['tindallgrams']
+      @opts = markup.split(/\s+/)
+      @filtered = filter(@grams, @opts[0], @opts[1])
+
+    end
+
+    def filter(grams, filter_type, filter_value)
+      if filter_type == 'year'
+        return grams.reject{|g| g['date'].strftime('%Y') != filter_value}
+      end
+
+      return grams
     end
 
     def render(context)
-      cfg = YAML.load_file('_data/tindallgrams-index.yaml')
-
-      # Make hash index of published pages
       published = Hash[context.registers[:site].pages.
         reject{|p| p.data['layout'] != 'tindallgram'}.
         map{|p| [p.data['serial'], p]}]
 
-      output = '<table class="tindallgram-index"><tbody>'
-      cfg['tindallgrams'].each do |tg|
+      # Make hash index of published pages
+
+      output = %Q|
+<table class="tindallgram-index">
+  <thead>
+    <th>Serial</th><th>Date</th><th>Subject</th><th>Sources</th>
+  </thead>
+  <tbody>|
+  
+      @filtered.each do |tg|
         if tg['date'] == nil
           datefmt = " "
         else 
           datefmt = tg['date'].strftime('%b %d %Y')
         end
 
+        row_open_tag = '<tr>'
         if published.keys.include?(tg['serial'])
+          row_open_tag = '<tr class="published">'
           link = published[tg['serial']].url.gsub(/\.html$/, '')
           subject = %Q|<a href="#{link}">#{tg['subject']}</a>|
         else
@@ -30,7 +49,7 @@ module Jekyll
         end
 
         output += %Q|
-<tr>
+#{row_open_tag}
   <td class="serial">#{tg['serial']}</td>
   <td class="date">#{datefmt}</td>
   <td class="subject">#{subject}</td>
