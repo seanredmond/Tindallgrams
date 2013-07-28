2d28b17a45d1571954cd0f5ad30c0a09e95612b1
Sean Redmond <github-smr@sneakemail.com>
2013-07-28 10:43:26 -0400

    Add gloassary page generator, and alpha index tag

--- _plugins/glossary_list.rb
+++ _plugins/glossary_list.rb
@@ -1,7 +1,57 @@
 require "redcarpet"
 
 module Jekyll
-  class GlossaryTag < Liquid::Tag
+  class GlossaryPage < Page
+    def initialize(site, base, dir, alpha, glosslist)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = "#{alpha}.html"
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'glossary_page.html')
+
+      self.data['title'] = "Glossary #{alpha}"
+      glosslist = glosslist.keys.sort{|a, b| a.downcase <=> b.downcase}.map{|k| glosslist[k]}
+      self.data['glosslist'] = glosslist
+    end
+  end
+
+  class GlossaryPageGenerator < Generator
+    def initialize(config)
+      super
+      @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
+      glossfile = YAML.load_file('_data/glossary.yaml')['glossary']
+      glossfile.each do |g|
+        if g.key?('def')
+          g['def'] = @markdown.render(g['def'])
+        elsif g.key?('cf')
+          g['def'] = @markdown.render("See #{g['cf']}")
+        end
+      end
+      @glossary = Hash[glossfile.map{|g| [g['term'], g]}]
+
+    end
+
+    def generate(site)
+      dir = 'terms'
+
+      #First generate a page for all terms that don't begin with a letter
+      by_letter = @glossary.reject{|k,v| k[0].downcase =~ /[a-z]/}
+      site.pages << GlossaryPage.new(site, site.source, dir, 'non-alpha', by_letter)
+
+      # Make a list of all the first characters of the glossary terms
+      letters = @glossary.keys.map{|a| a[0].downcase}.uniq
+
+      letters.reject{|l| l =~ /[^a-z]/}.each do |category|
+        by_letter = @glossary.reject{|k,v| k[0].downcase != category}
+        site.pages << GlossaryPage.new(site, site.source, dir, category, by_letter)
+      end
+    end
+  end
+
+
+  class GlossaryYamlTag < Liquid::Tag
     def initialize(tag_name, markup, tokens)
       glossfile = YAML.load_file('_data/glossary.yaml')['glossary']
       @glossary = Hash[glossfile.map{|g| [g['term'], g]}]
@@ -9,8 +59,9 @@ module Jekyll
 
       super
     end
+  end
 
-
+  class GlossaryTag < GlossaryYamlTag
     def matches(content)
       matches = @glossary.keys.map{|k| k if /\b#{k}\b/i =~ content}.reject{|g| g == nil}
       cfs = matches.map{|m| @glossary[m]['cf'] if @glossary[m]['cf'] != nil}.reject{|cf| cf == nil}
@@ -39,7 +90,25 @@ module Jekyll
 
     end
   end
+
+  class GlossaryAlphaTag < GlossaryYamlTag
+    def initialize(tag_name, markup, tokens)
+      super
+      @alpha = @glossary.keys.map{|a| a[0].downcase}.uniq.
+        reject{|a| a =~ /[^a-z]/}.sort
+      @alpha << 'non-alpha'
+    end
+
+    def render(context)
+      output = '<ul class="terms-alpha">'
+      @alpha.each do |a|
+        output += %Q|<li><a href="/terms/#{a}">#{a}</a></li>|
+      end
+      output += '</ul>'
+    end
+  end
 end
 
 Liquid::Template.register_tag('glossary', Jekyll::GlossaryTag)
+Liquid::Template.register_tag('glossary_alpha', Jekyll::GlossaryAlphaTag)
 
